325bfae579c7a6a3c74df6db9de2bd67a328968f
Ammar Yousuf <ammaryousuf10@gmail.com>
2026-04-10 19:18:40 +0500

    Completed GetProductById Endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Catalog.API.Products.GetProductById;
+
+//public record GetProductByIdRequest();
+
+public record GetProductByIdResponse(
+    Product Product
+);
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id:guid}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdRequest(id));
+            var response = result.Adapt<GetProductByIdResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .WithDescription("Get Product By Id")
+        .WithSummary("Get Product By Id")
+        .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,26 @@
+﻿
+using Catalog.API.Exceptions;
+
+namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdRequest(Guid Id) : IQuery<GetProductByIdResult>;
+public record GetProductByIdResult(Product Product);
+
+internal class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger) : IQueryHandler<GetProductByIdRequest, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdRequest query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@query}", query);
+
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+        if (product is null)
+        {
+            logger.LogWarning("Product with id {id} not found", query.Id);
+            throw new ProductNotFoundException();
+        }
+
+        var result = new GetProductByIdResult(product);
+        return result;
+    }
+}
